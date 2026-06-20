568400941173fba6a977b19ab51231f008392281
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:58:19 +0530

    Add files via upload

---
+++ User-Login-🔐.js
@@ -0,0 +1,8 @@
+let email = "user@example.com";
+let password = "pass123";
+
+if (email === "user@example.com" && password === "pass123") {
+    console.log("Login Successful");
+} else {
+    console.log("Invalid Email or Password!")
+}
