b52b1e647bcc774afe176716bd83ab6155ae3d3a
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:42:40 +0530

    Add files via upload

---
+++ practice14.js
@@ -0,0 +1,8 @@
+let accountVerified = true;
+let phoneVerified = true;
+
+if (accountVerified === true && phoneVerified === true) {
+    console.log("Account Setup Complete");
+} else {
+    console.log("Account Setup Incomplete")
+}
