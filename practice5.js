4a7d3025e43c0793ea389a08c66bbb7e284b0a31
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:18:24 +0530

    Add files via upload

---
+++ practice5.js
@@ -0,0 +1,8 @@
+let isLoggedIn = true;
+let isPremium = true;
+
+if (isLoggedIn === true && isPremium === true) {
+    console.log("Watch Premium Content");
+} else {
+    console.log("Upgrade Required")
+}
