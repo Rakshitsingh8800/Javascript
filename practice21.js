ac4dd19f9acde1b0c3b8c6869e6c74493b654767
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:55:46 +0530

    Add files via upload

---
+++ practice21.js
@@ -0,0 +1,8 @@
+let username = "root";
+let password = "admin123";
+
+if (username === "root" && password === "admin123") {
+    console.log("Access Granted");
+} else {
+    console.log("Access Denied")
+}
