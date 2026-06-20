8715e14717cac81a1042ec628911c4f42573d5d6
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:21:40 +0530

    Add files via upload

---
+++ practice6.js
@@ -0,0 +1,8 @@
+let username = "rajesh";
+let password = "pass123";
+
+if (username === "rajesh" && password === "pass123") {
+    console.log("Welcome!");
+} else {
+    console.log("Wrong Username or Password!")
+}
