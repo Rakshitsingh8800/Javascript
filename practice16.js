338f2701c62a68ed144927393b2f2e5010d7e021
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:46:17 +0530

    Add files via upload

---
+++ practice16.js
@@ -0,0 +1,8 @@
+let cameraPermission = true;
+let storagePermission = true;
+
+if (cameraPermission === true && storagePermission === true) {
+    console.log("Camera Ready");
+} else {
+    console.log("Permission Required!")
+}
