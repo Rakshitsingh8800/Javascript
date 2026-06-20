7b56bc2649a16a2e065535835291839dfaeb8e5c
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:06:08 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,8 @@
+let email = "abc@gmail.com";
+let otp = 5678;
+
+if (email === "abc@gmail.com" && otp === 5678) {
+    console.log("Verification Successful");
+} else {
+    console.log("Verification Failed")
+}
