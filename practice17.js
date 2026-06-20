c898e62913f0df297df76fae6f601654e09eb02d
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:48:01 +0530

    Add files via upload

---
+++ practice17.js
@@ -0,0 +1,8 @@
+let age = 25;
+let passport = true;
+
+if (age >= 18 && passport === true) {
+    console.log("Eligible for International Travel");
+} else {
+    console.log("Not Eligible for International Travel")
+}
