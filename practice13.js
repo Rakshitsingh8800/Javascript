dac95ea158180efcaea31d39b769f676dcc26a11
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:41:25 +0530

    Add files via upload

---
+++ practice13.js
@@ -0,0 +1,8 @@
+let hasTicket = true;
+let gateOpen = true;
+
+if (hasTicket === true && gateOpen === true) {
+    console.log("You can enter the event");
+} else {
+    console.log("Entry Denied")
+}
