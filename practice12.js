76d106d37971e520c1bc9e375aad4602c61322d0
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-20 22:39:58 +0530

    Add files via upload

---
+++ practice12.js
@@ -0,0 +1,8 @@
+let wifiConnected = true;
+let internetAvailable = true;
+
+if (wifiConnected === true && internetAvailable === true) {
+    console.log("You are online");
+} else {
+    console.log("You are offline");
+}
