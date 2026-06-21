99efa34c229bc1010bd929fb699a7cd962916542
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-21 15:28:04 +0530

    Add files via upload

---
+++ Rock-Scissor-Paper✂️.js
@@ -0,0 +1,8 @@
+function rps (user, computer) {
+    if (user === computer) return "draw";
+    if (user === "rock" && computer === "scissor") return "user";
+    if (user === "scissor" && computer === "paper") return "user";
+    if (user === "paper" && computer === "rock") return "user";
+    return "computer";
+}
+console.log(rps("scissor", "rock"));
