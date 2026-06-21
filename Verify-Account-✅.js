35103402b66227a785f38fb825c302a3253273ae
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-21 10:14:26 +0530

    Add files via upload

---
+++ Verify-Account-✅.js
@@ -0,0 +1,8 @@
+let emailVerified = true;
+let phoneVerified = true;
+
+if (emailVerified && phoneVerified) {
+    console.log("Account Verified");
+} else {
+    console.log("Verification Pending");
+}
