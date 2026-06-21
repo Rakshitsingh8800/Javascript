84dbd96c616083f3d651e88b78b9d0596756fe16
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-21 10:08:42 +0530

    Add files via upload

---
+++ Follow-a-User-👥.js
@@ -0,0 +1,8 @@
+let isLoggedIn = true;
+let targetUserExists = true;
+
+if (isLoggedIn === true && targetUserExists === true) {
+    console.log("You are now following the user.");
+} else {
+    console.log("You are not following the user.")
+}
