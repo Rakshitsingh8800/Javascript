2a39818869ef015a933adffec77870e3f15d5d20
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-21 15:55:07 +0530

    Add files via upload

---
+++ Weather-Advice☁️.js
@@ -0,0 +1,22 @@
+let weather = "rainy";
+
+switch (weather) {
+    case "sunny":
+        console.log("Wear light clothes and apply sunscreen.");
+        break;
+
+    case "rainy":
+        console.log("Wear a waterproof raincoat.");
+        break;
+
+    case "winter":
+        console.log("Wear warm clothes to protect yourself from cold weather.");
+        break;
+
+    case "snowy":
+        console.log("Wear a jacket.");
+        break;
+
+    default:
+        console.log("Invalid weather!");
+}
