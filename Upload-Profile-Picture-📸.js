af5fd06b8e72c687bcfd8e1a0afb5e42703486dc
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-21 10:10:32 +0530

    Add files via upload

---
+++ Upload-Profile-Picture-📸.js
@@ -0,0 +1,8 @@
+let isLoggedIn = true;
+let imageSelected = true;
+
+if (isLoggedIn === true && imageSelected === true) {
+    console.log("Profile Picture Updated");
+} else {
+    console.log("Profile picture update failed!")
+}
