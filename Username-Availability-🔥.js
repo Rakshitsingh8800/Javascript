35bbb602f5c4588092a2b77aca5f172a3c8c19e7
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-21 10:15:13 +0530

    Add files via upload

---
+++ Username-Availability-🔥.js
@@ -0,0 +1,8 @@
+let username = "neonex";
+let isAvailable = true;
+
+if (username !== "" && isAvailable) {
+    console.log("Username Available");
+} else {
+    console.log("Choose Another Username");
+}
