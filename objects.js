fa30f11696b3f9615f729bcbaef7dd266b34c21f
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-25 13:38:45 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,21 @@
+let obj = {
+  name: "Rakshit",
+  class: 8,
+  age: 14,
+  hobby: "Coding and Driving",
+  school: "Divine Happy School",
+  friends: ["Arpit", "Aman", "Aayush", "Saket"],
+  friendshobby: ["Arpit = Gaming", "Aman = Gaming", "Aayush = Gaming", "Saket = Studying and Gaming both"],
+  favoritefood: ["Pizza", "Burger", "Pasta", "Chowmein"],
+  favouritecolor: ["Black", "Pink", "Blue",],
+};
+
+console.log(obj.name);
+console.log(obj.class);
+console.log(obj.age);
+console.log(obj.hobby);
+console.log(obj.school);
+console.log(obj.friends);
+console.log(obj.friendshobby);
+console.log(obj.favoritefood);
+console.log(obj.favouritecolor);
