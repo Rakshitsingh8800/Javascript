4be6d1d138a7cabd51386795974c130657e4c30c
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-25 14:13:34 +0530

    Create object-entries.js

---
+++ object-entries.js
@@ -0,0 +1,13 @@
+let obj = {
+  name: "Rakshit",
+  class: 8,
+  age: 14,
+  hobby: "Coding and Driving",
+  school: "Divine Happy School",
+  friends: ["Arpit", "Aman", "Aayush", "Saket"],
+  friendshobby: ["Arpit = Gaming", "Aman = Gaming", "Aayush = Gaming", "Saket = Studying and Gaming both"],
+  favoritefood: ["Pizza", "Burger", "Pasta", "Chowmein"],
+  favouritecolor: ["Black", "Pink", "Blue",],
+};
+
+console.log (Object.entries(obj));
