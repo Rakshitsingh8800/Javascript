338d3bfba24e4cd3ee2388feef76b2a925fa0565
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-25 14:08:07 +0530

    Create For_in.js

---
+++ For_in.js
@@ -0,0 +1,15 @@
+let obj = {
+  name: "Rakshit",
+  class: 8,
+  age: 14,
+  hobby: "Coding and Driving",
+  school: "Divine Happy School",
+  friends: ["Arpit", "Aman", "Aayush", "Saket"],
+  friendshobby: ["Arpit = Gaming", "Aman = Gaming", "Aayush = Gaming", "Saket = Studying and Gaming both"],
+  favoritefood: ["Pizza", "Burger", "Pasta", "Chowmein"],
+  favouritecolor: ["Black", "Pink", "Blue",],
+};
+
+for (let key in obj) {
+    console.log(key, obj[key]);
+};
