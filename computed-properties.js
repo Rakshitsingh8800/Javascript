d1217456db4457df8edae29227ecec19bbfbbde2
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-25 15:05:23 +0530

    Create computed-properties.js

---
+++ computed-properties.js
@@ -0,0 +1,12 @@
+let role = "admin";
+
+let obj = {
+    name: "Rakshit",
+    age: 14,
+    email: "singhrakshitraj456@gmail.com",
+    address: {
+        city: "Gurgaon, Haryana",
+    },
+    [role]: "Rakshit",
+};
+
