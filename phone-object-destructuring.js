29f4348b484733a87a8f63feee4c5557814c99d4
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-26 12:56:56 +0530

    Create phone-object-destructuring.js

---
+++ phone-object-destructuring.js
@@ -0,0 +1,18 @@
+let phone = {
+    brand: "Apple",
+    model: "iPhone 17 Pro",
+    storage: "512GB",
+    color: "Black"
+};
+
+let {
+    brand: brandname,
+    model: modelname,
+    storage: storagename,
+    color: colorname
+} = phone;
+
+console.log(brandname);
+console.log(modelname);
+console.log(storagename);
+console.log(colorname);
