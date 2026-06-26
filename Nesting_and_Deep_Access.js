824ee1ecb77daf5fd9cd16128fe638342cc2dad3
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-26 12:36:37 +0530

    Create Nesting_and_Deep_Access.js

---
+++ Nesting_and_Deep_Access.js
@@ -0,0 +1,10 @@
+let student = {
+    name: "Rajesh",
+    school: {
+        name: "DAV Public School",
+        city: "Bhagalpur"
+    }
+};
+console.log(student.name);
+console.log(student.school.name);
+console.log(student.school.city);
