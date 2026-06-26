59f85ff04954896d4aacedbadabc5751b6a4eab8
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-26 13:33:09 +0530

    Create employee-optionalchaning.js

---
+++ employee-optionalchaning.js
@@ -0,0 +1,9 @@
+let employee = {
+    name: "Rajesh",
+    department: {
+        name: "Software Development"
+    }
+};
+
+console.log(employee.department.name);
+console.log(employee?.department?.manager);
