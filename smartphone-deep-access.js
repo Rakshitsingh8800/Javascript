66b3795fc28f8bcadc19c6078924310e6020a93a
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-26 12:49:20 +0530

    Create smartphone-deep-access.js

---
+++ smartphone-deep-access.js
@@ -0,0 +1,19 @@
+let smartphone = {
+    brand: "Samsung",
+    specifications: {
+        display: {
+            size: "6.7 inch",
+            type: "AMOLED"
+        },
+        battery: {
+            capacity: "5000mAh",
+            charging: "45W"
+        }
+    }
+};
+
+console.log(smartphone.brand);
+console.log(smartphone.specifications.display.size);
+console.log(smartphone.specifications.display.type);
+console.log(smartphone.specifications.battery.capacity);
+console.log(smartphone.specifications.battery.charging);
