3d44a0a7f4c818ce855358169e78fe4e02f0da4f
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-26 12:52:26 +0530

    Create laptop-object-destructuring.js

---
+++ laptop-object-destructuring.js
@@ -0,0 +1,11 @@
+let laptop = {
+    brand: "ASUS",
+    ram: "16GB",
+    processor: "Intel Core i5"
+};
+
+let {brand,ram,processor} = laptop;
+
+console.log(brand);
+console.log(ram);
+console.log(processor);
