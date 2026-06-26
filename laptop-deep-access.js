f34a275a7c7ee1e7878b5229dc0529a34b44e6c1
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-06-26 12:40:45 +0530

    Create laptop-deep-access.js

---
+++ laptop-deep-access.js
@@ -0,0 +1,15 @@
+let laptop = {
+    brand: "ASUS",
+    specifications: {
+        processor: {
+            name: "Intel Core i5",
+            generation: "13th Gen"
+        },
+        ram: "16GB"
+    }
+};
+
+console.log(laptop.brand);
+console.log(laptop.specifications.processor.name);
+console.log(laptop.specifications.processor.generation);
+console.log(laptop.specifications.ram);
